d188c016252a019efb43d72142c2efa27b71d8b4
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2024-08-12 16:42:13 +0100

    create movie model

---
+++ backend/src/movie/movie.model.ts
@@ -0,0 +1,26 @@
+import { Duration } from "./dto/movie-request.dto";
+
+export class Movie {
+     title: string;
+     year: string;
+     duration: Duration;
+     actors: string;
+     language: string;
+     genre: string;
+     cover: string;
+
+     constructor(data: any) {
+        this.title = data.title;
+        this.year = data.year;
+        this.duration = data.duration;
+        this.actors = data.actors;
+        this.language = data.language;
+        this.genre = data.genre;
+        this.cover = data.cover 
+     }
+
+    static fromJson(json: string): Movie {
+        const data = JSON.parse(json);
+        return new Movie(data);
+    }
+}
