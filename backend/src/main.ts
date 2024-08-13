8e81c9ee75f6fd5137f6291c18f10f7e9c562909
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2024-08-13 07:56:43 +0100

    initialise pieces

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as Pieces from '@pieces.app/pieces-os-client';
+import os from 'os';
+
 
 async function bootstrap() {
+  let port = 1000;
+
+  const configuration = new Pieces.Configuration({
+    basePath: `http://localhost:${port}`
+  })
+
+  const apiInstance = new Pieces.WellKnownApi(configuration)
+
+  apiInstance.getWellKnownHealth().then((data: string) => {
+    console.log(data) // ok
+  }).catch((error: unknown) => console.error(error));
+
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
 }
 bootstrap();
