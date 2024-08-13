1bc2b5c0d9ac13753e26e03ce6992a06f1ea5cfa
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2024-08-13 08:02:40 +0100

    create movies controller

--- backend/src/movie/movie.controller.ts
+++ backend/src/movie/movie.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { MovieService } from './movie.service';
+import { MovieRequestDto } from './dto/movie-request.dto';
+import { Movie } from './movie.model';
+import { Response } from 'express';
 
-@Controller('movie')
-export class MovieController {}
+@Controller('movies')
+export class MovieController {
+
+    constructor(private readonly movieService: MovieService) {}
+
+
+    @Post("recommendations")
+    async getMovieRecommendations(@Body() movieRequestDto: MovieRequestDto, @Res() res: Response) {
+        try {
+            const movies: Movie[] = await this.movieService.getMovieRecommendations(movieRequestDto);
+            res.status(HttpStatus.OK).json(movies);
+        } catch (Error) {
+            console.log(Error)
+            
+            res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+                statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+                message: "Failed to get movie recommendations"
+            })
+        }
+        
+    }
+}
