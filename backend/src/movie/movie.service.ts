2ad7ad2e6cc5c3bd5cd33779c4f194fbe7152dc9
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2024-08-13 08:01:46 +0100

    create movie service

--- backend/src/movie/movie.service.ts
+++ backend/src/movie/movie.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { PiecesService } from 'src/pieces/pieces.service';
+import { MovieRequestDto } from './dto/movie-request.dto';
+import { Movie } from './movie.model';
 
 @Injectable()
-export class MovieService {}
+export class MovieService {
+    constructor(private readonly piecesService: PiecesService) {}
+
+    async getMovieRecommendations(movieRequestDto: MovieRequestDto): Promise<Movie[]> {
+        try {
+            const response = (await this.piecesService.postMovieRequest(movieRequestDto)).replace("```json", "").replace("```", "");
+
+            const moviesData = JSON.parse(response);
+
+            if (Array.isArray(moviesData)) {
+                return moviesData.map((movieData: any) => new Movie(movieData));
+            } else {
+                throw new Error("Unexpected response format");
+            }
+        } catch (error) {
+            console.log(error);
+            throw new Error("Failed to get movie recommendations");
+        }
+    }
+}
