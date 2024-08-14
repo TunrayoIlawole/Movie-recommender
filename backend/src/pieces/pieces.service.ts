c242cdc607c4e4b9f6c262a5cf2320edaff67c51
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2024-08-14 17:08:30 +0100

    get duration value

--- backend/src/pieces/pieces.service.ts
+++ backend/src/pieces/pieces.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PiecesClient } from 'pieces-copilot-sdk';
 import { Observable } from 'rxjs';
 import { MovieRequestDto } from 'src/movie/dto/movie-request.dto';
+import { Duration } from 'src/movie/movie.model';
 
 
 @Injectable()
@@ -15,11 +16,25 @@ export class PiecesService {
         });
     }
 
-    async postMovieRequest(movieRequestDto: MovieRequestDto): Promise<string> {
-        this.movieRequestString = `Please suggest ${movieRequestDto.language} movies of ${movieRequestDto.genre} and duration ${movieRequestDto.duration} starring ${movieRequestDto.actor} with their titles, year, duration, actors, language, genre and movie covers in JSON format`;
+    async sendMovieRequest(movieRequestDto: MovieRequestDto): Promise<string> {
+        this.movieRequestString = `Please suggest ${movieRequestDto.language === null ? "": movieRequestDto.language} movies of ${movieRequestDto.genre} genre and duration of ${this.getDurationString(movieRequestDto.duration)} starring ${movieRequestDto.actor} with their titles, year, duration, actors, language, genre and movie covers in JSON format`;
+        console.log(this.movieRequestString);
         return this.piecesClient.askQuestion({
             question: this.movieRequestString
         });
 
+    }
+
+    getDurationString(duration: string): string {
+        let durationValue = "";
+        if (duration === "SHORT") {
+            durationValue = Duration.SHORT;
+        } else if (duration === "Medium") {
+            durationValue = Duration.MEDIUM;
+        } else {
+            durationValue = Duration.LONG;
+        }
+
+        return durationValue;
     }
 }
