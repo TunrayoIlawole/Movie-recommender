a4b4e96c79350bb9421f43e46bb8cac530688ada
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2024-08-14 20:28:32 +0100

    refactor code

--- backend/src/movie/movie.service.ts
+++ backend/src/movie/movie.service.ts
@@ -3,26 +3,34 @@ import { PiecesService } from 'src/pieces/pieces.service';
 import { MovieRequestDto } from './dto/movie-request.dto';
 import { Movie } from './movie.model';
 
+interface MovieData {
+    movies: Movie[];
+}
+
 @Injectable()
 export class MovieService {
     constructor(private readonly piecesService: PiecesService) {}
 
     async getMovieRecommendations(movieRequestDto: MovieRequestDto): Promise<Movie[]> {
         try {
-            const response = (await this.piecesService.sendMovieRequest(movieRequestDto)).replace("```json", "").replace("```", "");
+            const response = await this.piecesService.sendMovieRequest(movieRequestDto);
 
-            const moviesData = JSON.parse(response);
+            const moviesData: MovieData = JSON.parse(response);
 
             if (Array.isArray(moviesData.movies)) {
-                return moviesData.movies.map((movieData: any) => new Movie(movieData));
+                return moviesData.movies.map((movieData) => new Movie(movieData));
             } else {
                 throw new Error("Error: Unexpected movie response format");
             }
         } catch (error) {
-            console.log(error);
             const errorCode = error.code;
             const errorMessage = error.message;
+            console.error(`Error fetching movie recommendations: ${error}`)
             throw new Error(`Error: ${errorCode} - ${errorMessage}`);
         }
+    }
+
+    private cleanMoviesResponse(response: string): string {
+        return response.replace(/```json|```/g, "").trim();
     }
 }
